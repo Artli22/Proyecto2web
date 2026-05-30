a532f651a659edb2a4d2dab328f66cd981a743a5
Artli22 <ludinlima2514@gmail.com>
2026-05-30 11:55:17 -0600

    Preparativos vite+storybook+lint

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import { defineConfig, globalIgnores } from 'eslint/config'
+
+export default defineConfig([globalIgnores(['dist']), {
+  files: ['**/*.{js,jsx}'],
+  extends: [
+    js.configs.recommended,
+    reactHooks.configs.flat.recommended,
+    reactRefresh.configs.vite,
+  ],
+  languageOptions: {
+    globals: globals.browser,
+    parserOptions: { ecmaFeatures: { jsx: true } },
+  },
+  rules: {
+    semi: ['error', 'never'],
+    'max-len': ['error', { code: 120 }],
+  },
+}, ...storybook.configs["flat/recommended"]])
